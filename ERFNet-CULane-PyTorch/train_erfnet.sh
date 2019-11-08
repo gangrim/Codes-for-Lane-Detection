4a6de4349cc20cb7cd8fe45e1577938d92dc0989
gangrim <gis20100101@outlook.com>
2019-11-09 00:46:09 +0900

    add

--- ERFNet-CULane-PyTorch/train_erfnet.sh
+++ ERFNet-CULane-PyTorch/train_erfnet.sh
@@ -1,6 +1,6 @@
 python3 -u train_erfnet.py CULane ERFNet train_gt val_gt \
                         --lr 0.01 \
-                        --gpus 0 \
+                        --gpus 1 \
                         --npb \
                         --resume none \
                         -j 12 \
